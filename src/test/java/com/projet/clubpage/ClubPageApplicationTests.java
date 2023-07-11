36c97ea6f5c3ba909b713f87a06c6738467615cc
no.oneho <99154108+no-oneho@users.noreply.github.com>
2023-07-11 21:46:24 +0900

    init commit
    프로젝트 초기 세팅 :D

---
+++ src/test/java/com/projet/clubpage/ClubPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.clubpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
