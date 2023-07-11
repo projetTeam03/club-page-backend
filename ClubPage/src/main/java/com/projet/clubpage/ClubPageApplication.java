07f499298a6daa75f02f513fceb0ce59d12375c3
no.oneho <99154108+no-oneho@users.noreply.github.com>
2023-07-11 21:44:32 +0900

    project init
    DB연결한 프로젝트 초기 세팅입니다.

---
+++ ClubPage/src/main/java/com/projet/clubpage/ClubPageApplication.java
@@ -0,0 +1,13 @@
+package com.projet.clubpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubPageApplication.class, args);
+    }
+
+}
